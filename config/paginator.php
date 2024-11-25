f6ccfb1550de4384bec5fdcfdfa269c780a70087
calesj <cales.junior2002@gmail.com>
2024-11-25 13:54:05 -0300

    feat - adicionando o paginator

---
+++ config/paginator.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'first' => '<li class="page-item"><a href="{{url}}" class="page-link">Primeira</a></li>',
+    'current' => '<li class="page-item active"><a class="page-link">{{text}}</a></li>',
+    'number' => '<li class="page-item"><a href="{{url}}" class="page-link">{{text}}</a></li>',
+    'nextActive' => '<li class="page-item"><a href="{{url}}" class="page-link">Proximo</a></li>',
+    'prevActive' => '<li class="page-item"><a href="{{url}}" class="page-link">Anterior</a></li>',
+    'prevDisabled' => '',
+    'nextDisabled' => '',
+    'last' => '<li class="page-item"><a href="{{url}}" class="page-link">Ultima</a></li>',
+];
