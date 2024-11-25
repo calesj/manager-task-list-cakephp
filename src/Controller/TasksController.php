23c2f459c1b9d02ffdc450bcb51bae68192e43dd
calesj <cales.junior2002@gmail.com>
2024-11-25 13:55:40 -0300

    feat - models, entities, tables e controllers

---
+++ src/Controller/TasksController.php
@@ -0,0 +1,199 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Cake\Http\CallbackStream;
+use Cake\Http\Response;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use Psr\Http\Message\MessageInterface;
+
+/**
+ * Tasks Controller
+ *
+ * @property \App\Model\Table\TasksTable $Tasks
+ */
+class TasksController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function index()
+    {
+        $query = $this->Tasks->find();
+
+        /** usuario logado */
+        $user = $this->Authentication->getIdentity();
+
+        if ($this->request->getQuery('title')) {
+            $title = $this->request->getQuery('title');
+            $query->where(['title LIKE "%' . $title . '%"', 'content LIKE "%' . $title . '%"']);
+        }
+
+        if ($this->request->getQuery('status')) {
+            $status = $this->request->getQuery('status');
+            $query->where(['status' => $status]);
+        }
+
+        if ($this->request->getQuery('date')) {
+            $date = $this->request->getQuery('date');
+            $query->where(['deadline' => $date]);
+        }
+
+        $tasks = $this->paginate($query->where(['user_id' => $user->id]), [
+            'limit' => 7,
+            'order' => ['id' => 'desc'],
+        ]);
+
+        $this->set(compact('tasks', 'user'));
+
+        return $this->render('/Home/index', 'master');
+    }
+
+    /**
+     * @return \Cake\Http\Response
+     */
+    public function create(): Response
+    {
+        /** usuario logado */
+        $user = $this->Authentication->getIdentity();
+
+        $task = $this->Tasks->newEmptyEntity();
+
+        /** Se for uma requisicao post */
+        if ($this->request->is('post')) {
+            $task = $this->Tasks->patchEntity($task, $this->request->getData());
+            $task->user_id = $user->id;
+            if ($this->Tasks->save($task)) {
+                $this->Flash->success(__('Task Salva com sucesso'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The task could not be saved. Please, try again.'));
+        }
+
+        $this->set(compact('user', 'task'));
+
+        return $this->render('/Home/create', 'master');
+    }
+
+    /**
+     * @return \Cake\Http\Response|\Psr\Http\Message\MessageInterface
+     */
+    public function export(): MessageInterface|Response
+    {
+        $this->autoRender = false;
+
+        $tasks = $this->Tasks->find('all');
+
+        // Criar planilha
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+
+        // Preenchendo os dados
+        $sheet->setCellValue('A1', 'ID')
+            ->setCellValue('B1', 'Título')
+            ->setCellValue('C1', 'Descrição')
+            ->setCellValue('D1', 'Status')
+            ->setCellValue('E1', 'Prazo');
+
+        $row = 2;
+        foreach ($tasks as $task) {
+            $sheet->setCellValue('A' . $row, $task->id)
+                ->setCellValue('B' . $row, $task->title)
+                ->setCellValue('C' . $row, $task->content)
+                ->setCellValue('D' . $row, $task->status)
+                ->setCellValue('E' . $row, $task->deadline->i18nFormat('dd/MM/yyyy'));
+            $row++;
+        }
+
+        // Criar escritor Excel
+        $writer = new Xlsx($spreadsheet);
+
+        // Save the file in a stream
+        $stream = new CallbackStream(function () use ($writer) {
+            $writer->save('php://output');
+        });
+
+        $filename = 'tasks';
+        $response = $this->response;
+
+        return $response->withType('xlsx')
+            ->withHeader('Content-Disposition', "attachment;filename=\"{$filename}.xlsx\"")
+            ->withBody($stream);
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $task = $this->Tasks->newEmptyEntity();
+
+        /** Se for uma requisicao post */
+        if ($this->request->is('post')) {
+            $task = $this->Tasks->patchEntity($task, $this->request->getData());
+            if ($this->Tasks->save($task)) {
+                $this->Flash->success(__('The task has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The task could not be saved. Please, try again.'));
+        }
+
+        /** Se for uma requisicao GET */
+        $users = $this->Tasks->Users->find('list', limit: 200)->all();
+        $this->set(compact('task', 'users'));
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Task id.
+     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
+     * @throws \App\Controller\RecordNotFoundException When record not found.
+     */
+    public function edit(?string $id = null)
+    {
+        $user = $this->Authentication->getIdentity();
+        $task = $this->Tasks->get($id, contain: []);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $task = $this->Tasks->patchEntity($task, $this->request->getData());
+            if ($this->Tasks->save($task)) {
+                $this->Flash->success(__('The task has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The task could not be saved. Please, try again.'));
+        }
+
+        $this->set(compact('task', 'user'));
+
+        return $this->render('/Home/edit', 'master');
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Task id.
+     * @return \Cake\Http\Response|null Redirects to index.
+     * @throws \App\Controller\RecordNotFoundException When record not found.
+     */
+    public function delete(?string $id = null): ?Response
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $task = $this->Tasks->get($id);
+        if ($this->Tasks->delete($task)) {
+            $this->Flash->success(__('The task has been deleted.'));
+        } else {
+            $this->Flash->error(__('The task could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+}
---
+++ src/Model/Entity/Task.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * Task Entity
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $content
+ * @property string $status
+ * @property \Cake\I18n\Date|null $deadline
+ * @property int $user_id
+ * @property \Cake\I18n\DateTime $created_at
+ * @property \Cake\I18n\DateTime $updated_at
+ *
+ * @property \App\Model\Entity\User $user
+ */
+class Task extends Entity
+{
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array<string, bool>
+     */
+    protected array $_accessible = [
+        'title' => true,
+        'content' => true,
+        'status' => true,
+        'deadline' => true,
+        'user_id' => true,
+        'created_at' => true,
+        'updated_at' => true,
+        'user' => true,
+    ];
+}
